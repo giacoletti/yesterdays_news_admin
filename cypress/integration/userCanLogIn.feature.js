ba79a439b3b153547d8ed255da19c0a107567121
giacoletti <giacoletti92@gmail.com>
2022-01-21 15:34:32 +0100

    Changes end2end test to check if user is able to authenticate

--- cypress/integration/userCanLogIn.feature.js
+++ cypress/integration/userCanLogIn.feature.js
@@ -1,7 +1,11 @@
 describe("A journalist is able to login to article creation form", () => {
   before(() => {
-    cy.intercept("POST", "api/auth", {
+    cy.intercept("POST", "api/auth/sign_in", {
       fixture: "login_successful.json",
+    }).as('authenticatedResponse');
+    cy.intercept("GET", "/api/auth/validate_token", {
+      fixture: "authenticated_journalist_response.json",
+      headers: { uid: "johnskoglung@test.com", token: "12344556789" },
     });
     cy.visit("/");
     cy.get("[data-cy=login-email-input]").type("johnskoglung@test.com");
@@ -9,7 +13,11 @@ describe("A journalist is able to login to article creation form", () => {
     cy.get("[data-cy=login-button]").click();
   });
 
-  it("is expected to match email input with uid from API", () => {
-    
-  })
+  it('is expected to make an authentication request to API', () => {
+    cy.wait('@authenticatedResponse').its('response.statusCode').should('eq', 200);
+  });
+
+  it('is expected to display a welcome message', () => {
+    cy.get("[data-cy=flash-message]").should("contain.text", "Welcome John Skoglund!");
+  });
 });
