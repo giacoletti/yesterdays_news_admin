f1be2a0fc3fb1777abe9f5ff89f08c389f91d93f
Elvita Kalvāne <elvita.kalvane@gmail.com>
2022-01-23 15:45:14 +0100

    extract url and its conditions to a separate file apiURL

--- src/modules/network.js
+++ src/modules/network.js
@@ -1,11 +1,4 @@
 import axios from "axios";
-
-let apiURL;
-
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  apiURL = "http://localhost:3000/api";
-} else {
-  apiURL = "https://yesterdays-news-api.herokuapp.com/api";
-}
+import apiURL from "./apiURL";
 
 export const api = axios.create({ baseURL: apiURL });
