a0658ea0cac6832d0cb1598d4850426f21bf30c9
Elvita Kalvāne <elvita.kalvane@gmail.com>
2022-01-23 13:59:05 +0100

    update url to deployed api url add condition to url

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,9 +1,17 @@
 import JtockAuth from "j-tockauth";
- 
+
+let hostURL;
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  hostURL = "http://localhost:3000";
+} else {
+  hostURL = "https://yesterdays-news-api.herokuapp.com";
+}
+
 const auth = new JtockAuth({
-  host: "http://localhost:3000",
+  host: hostURL,
   prefixUrl: "/api",
-  debug: false
+  debug: false,
 });
- 
+
 export default auth;
