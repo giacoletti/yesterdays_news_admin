07bcd6ddbf1f617cdf4c1cc7c9504ae674c0f91e
giacoletti <giacoletti92@gmail.com>
2022-01-30 13:20:19 +0100

    WIP refactoring end2end test to check if journalist receives error messages when writing incomplete articles

--- cypress/integration/journalistReceivesMessageNotCreatedDueToMissingParams.feature.js
+++ cypress/integration/journalistReceivesMessageNotCreatedDueToMissingParams.feature.js
@@ -1,11 +1,45 @@
 /* eslint-disable no-undef */
 describe("Journalist can receive an error message if article is not created due to missing params", () => {
+  
+  // before(() => {
+  //   cy.intercept("POST", "/api/auth/sign_in", {
+  //     fixture: "authenticated_journalist_response.json",
+  //   }).as("authenticateRequest");
+  //   cy.intercept("GET", "/api/auth/validate_token", {
+  //     fixture: "authenticated_journalist_response.json",
+  //     headers: { uid: "johnskoglung@test.com", token: "12344556789" },
+  //   }).as("validateTokenRequest");
+  //   cy.intercept("POST", "/api/articles", {
+  //     fixture: "create_response.json",
+  //   }).as("articleCreateRequest");
+  //   cy.visit("/");
+  //   cy.get("[data-cy=login-email-input]").type("johnskoglung@test.com");
+  //   cy.get("[data-cy=login-password-input]").type("1234567890");
+  //   cy.get("[data-cy=login-button]").click();
+  //   cy.get("[data-cy=create-article-btn]").click();
+  // });
+  
   describe("Missing title", () => {
     before(() => {
+      cy.intercept("POST", "/api/auth/sign_in", {
+        fixture: "authenticated_journalist_response.json",
+      }).as("authenticateRequest");
+      cy.intercept("GET", "/api/auth/validate_token", {
+        fixture: "authenticated_journalist_response.json",
+        headers: { uid: "johnskoglung@test.com", token: "12344556789" },
+      }).as("validateTokenRequest");
+      cy.intercept("POST", "/api/articles", {
+        fixture: "create_response.json",
+      }).as("articleCreateRequest");
+      cy.visit("/");
+      cy.get("[data-cy=login-email-input]").type("johnskoglung@test.com");
+      cy.get("[data-cy=login-password-input]").type("1234567890");
+      cy.get("[data-cy=login-button]").click();
+      cy.get("[data-cy=create-article-btn]").click();
       cy.intercept("POST", "/api/articles", {
         fixture: "title_is_missing.json",
       });
-      cy.visit("article/create");
+      // cy.visit("article/create");
       cy.get("[data-cy=body-input]").type(
         "New findings on Björkö shows that Vikings enjoyed Pineapple pizza"
       );
@@ -23,10 +57,24 @@ describe("Journalist can receive an error message if article is not created due 
 
   describe("Missing body", () => {
     before(() => {
+      cy.intercept("POST", "/api/auth/sign_in", {
+        fixture: "authenticated_journalist_response.json",
+      }).as("authenticateRequest");
+      cy.intercept("GET", "/api/auth/validate_token", {
+        fixture: "authenticated_journalist_response.json",
+        headers: { uid: "johnskoglung@test.com", token: "12344556789" },
+      }).as("validateTokenRequest");
+      cy.intercept("POST", "/api/articles", {
+        fixture: "create_response.json",
+      }).as("articleCreateRequest");
+      cy.visit("/");
+      cy.get("[data-cy=login-email-input]").type("johnskoglung@test.com");
+      cy.get("[data-cy=login-password-input]").type("1234567890");
+      cy.get("[data-cy=login-button]").click();
+      cy.get("[data-cy=create-article-btn]").click();
       cy.intercept("POST", "/api/articles", {
         fixture: "body_is_missing.json",
       });
-      cy.visit("article/create");
       cy.get("[data-cy=title-input]").type("Vikings ate pizza");
       cy.get("[data-cy=category-select]").click();
       cy.get("[data-cy=news-category]").click();
@@ -42,10 +90,24 @@ describe("Journalist can receive an error message if article is not created due 
 
   describe("Missing category", () => {
     before(() => {
+      cy.intercept("POST", "/api/auth/sign_in", {
+        fixture: "authenticated_journalist_response.json",
+      }).as("authenticateRequest");
+      cy.intercept("GET", "/api/auth/validate_token", {
+        fixture: "authenticated_journalist_response.json",
+        headers: { uid: "johnskoglung@test.com", token: "12344556789" },
+      }).as("validateTokenRequest");
+      cy.intercept("POST", "/api/articles", {
+        fixture: "create_response.json",
+      }).as("articleCreateRequest");
+      cy.visit("/");
+      cy.get("[data-cy=login-email-input]").type("johnskoglung@test.com");
+      cy.get("[data-cy=login-password-input]").type("1234567890");
+      cy.get("[data-cy=login-button]").click();
+      cy.get("[data-cy=create-article-btn]").click();
       cy.intercept("POST", "/api/articles", {
         fixture: "category_is_missing.json",
       });
-      cy.visit("article/create");
       cy.get("[data-cy=title-input]").type("Vikings ate pizza");
       cy.get("[data-cy=body-input]").type(
         "New findings on Björkö shows that Vikings enjoyed Pineapple pizza"
